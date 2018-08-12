3db9dc947bfd23d56fbcde61177252c44c25a787
lemonliu1993 <2305247091@qq.com>
2018-08-13 00:19:14 +0800

    controller的使用

---
+++ src/main/java/com/lemon/demo/GirlProperties.java
@@ -0,0 +1,37 @@
+package com.lemon.demo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by lemoon on 18/8/12 下午1:05
+ */
+
+
+/**
+ * 要想加入配置还需要Component注解
+ */
+@Component
+@ConfigurationProperties(prefix = "girl")
+public class GirlProperties {
+
+    private String cupSize;
+
+    private Integer age;
+
+    public String getCupSize() {
+        return cupSize;
+    }
+
+    public void setCupSize(String cupSize) {
+        this.cupSize = cupSize;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
--- src/main/java/com/lemon/demo/HelloController.java
+++ src/main/java/com/lemon/demo/HelloController.java
@@ -1,25 +1,39 @@
 package com.lemon.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+//@Controller
+@RequestMapping("/hello")
 public class HelloController {
 
-    //通过注解读取配置属性,格式"${name}",要加$
-    @Value("${cupSize}")
-    private String cupSize;
+//    //通过注解读取配置属性,格式"${name}",要加$
+//    @Value("${cupSize}")
+//    private String cupSize;
+//
+//    @Value("${age}")
+//    private Integer age;
+//
+//    @Value("${content}")
+//    private String content;
 
-    @Value("${age}")
-    private Integer age;
+    @Autowired
+    private GirlProperties girlProperties;
 
-    @Value("${content}")
-    private String content;
+//    @RequestMapping(value = {"/hello","hi"} ,method = RequestMethod.GET)
+    @RequestMapping(value = "/say/{id}" ,method = RequestMethod.GET)
+    public String say(@PathVariable("id") Integer id){
+//        return girlProperties.getCupSize();
+//        return "index";
+        return "id:" +id;
+    }
 
-    @RequestMapping(value = "hello" ,method = RequestMethod.GET)
-    public String say(){
-        return content;
+//    @RequestMapping(value = "/hi" ,method = RequestMethod.GET)
+    @GetMapping
+    public String hi(@RequestParam(value = "id", required = false,defaultValue = "0") Integer myId){
+        return "id:" +myId;
     }
 }
